60f9d6f3bffb5d2002134f5f9a91d47580a225b4
Aboubakar Koïta <aboubakar.koita@yahoo.fr>
2024-11-06 12:06:16 +0100

    init

--- src/Foo.sol
+++ src/Foo.sol
@@ -1,8 +1,6 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity 0.8.28;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-
 contract Foo {
     function id(uint256 value) external pure returns (uint256) {
         return value;
